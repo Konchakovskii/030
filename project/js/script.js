3bea28ceb9d9fd9e64adba72bce12d0d8f404ec5
Igor Konchakovskii <northernfenian@gmail.com>
2023-10-18 17:09:33 +0300

    second

--- project/js/script.js
+++ project/js/script.js
@@ -14,7 +14,8 @@
 
 'use strict';
 
-const adv = document.querySelectorAll('.promo__adv img'),
+document.addEventListener('DOMContentLoaded', () => {
+    const adv = document.querySelectorAll('.promo__adv img'),
     poster = document.querySelector('.promo__bg'),
     genre = poster.querySelector('.promo__genre'),
     movieList = document.querySelector('.promo__interactive-list');
@@ -51,6 +52,28 @@ movieDB.movies.sort();
 //     filmList[i].innerHTML = `<li>${movieDB.movies[i]}</li>`;
 // }
 
+
+
+const form = document.querySelector('.add');
+
+const formBtn = form.querySelector('button');
+
+formBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let movieName = form.querySelector('.adding__input');
+    if (movieName.value.length > 21) {
+        movieName.value = movieName.value.slice(0, 21) + '...';
+    }
+    movieDB.movies.push(movieName.value);
+    console.log(movieDB.movies);
+
+    movieList.innerHTML += `
+         <li class="promo__interactive-item">${movieDB.movies.length} ${movieName.value}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
 movieDB.movies.forEach((film, i) => {
     movieList.innerHTML += `
          <li class="promo__interactive-item">${i + 1} ${film}
@@ -58,3 +81,17 @@ movieDB.movies.forEach((film, i) => {
         </li>
     `;
 });
+
+
+const items = document.querySelectorAll('.promo__interactive-list');
+
+items.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.target.remove();
+    });
+
+});
+
+
+});
+
